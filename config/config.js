eb8493723856a6f75c354235735a249afdd80783
yangDaXia <2215890610@qq.com>
2019-01-30 23:21:01 +0800

    更新配置

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 import pageRoutes from "./router.config";
 
+import theme from "./theme";
 
 const plugins = [
   // ref: https://umijs.org/plugin/umi-plugin-react.html
@@ -8,7 +9,7 @@ const plugins = [
     dva: true, //https://umijs.org/zh/plugin/umi-plugin-react.html#dva
     title: 'FCT后台管理系统',
     dll: true, //https://umijs.org/zh/plugin/umi-plugin-react.html#dll
-    routes: {
+    routes: { //https://umijs.org/zh/plugin/umi-plugin-react.html#antd
       exclude: [],
     }, 
     hardSource: false, //https://umijs.org/zh/plugin/umi-plugin-react.html#hardsource
@@ -28,7 +29,18 @@ const plugins = [
 export default {
   plugins: plugins,
   disableRedirectHoist: true, //详细见 https://umijs.org/zh/config/#disableredirecthoist
-  routes: pageRoutes,
+  routes: pageRoutes, // https://umijs.org/zh/guide/router.html#%E9%85%8D%E7%BD%AE%E5%BC%8F%E8%B7%AF%E7%94%B1
   // history: 'hash', // 配置hash路由 https://umijs.org/zh/config/#history
   // base:"", //https://umijs.org/zh/config/#base
+  // theme: { // 定制UI // https://ant.design/docs/react/customize-theme-cn
+  //   'primary-color': "#202020",
+  // },
+  theme: theme,
+
+  externals: { // 配置不打包进入dist的npm  https://umijs.org/zh/config/#externals
+    // '@antv/data-set': 'DataSet',
+  },
+  ignoreMomentLocale: true, //忽略 moment 的 locale 文件，用于减少尺寸。 https://umijs.org/zh/config/#ignoremomentlocale
+  disableRedirectHoist: true, //https://umijs.org/zh/config/#disableredirecthoist
+
 }
--- src/routes/RootRoute.js
+++ src/routes/RootRoute.js
@@ -4,12 +4,13 @@ import router from 'umi/router';
 class RootRoute extends React.Component{
 
   componentDidMount = ()=>{
+    debugger
     router.push('/login');
   }
   render (){
     return (
       <div>
-
+          cscdcdcd
       </div>
     )
   }
