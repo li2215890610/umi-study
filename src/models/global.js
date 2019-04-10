ce4b540a58415058b82cab76b617671341e9fd47
yangDaXia <2215890610@qq.com>
2019-04-10 11:13:29 +0800

    udpate

--- src/models/global.js
+++ src/models/global.js
@@ -12,7 +12,6 @@ export default {
     //history 监听路由变化、dispatch 触发 effects与reducers 的方法
     setup({ dispatch, history }) {
       history.listen(({ pathname }) => {  
-        console.log(pathname);
 
         if (pathname === '/') {
           console.log("/global")
