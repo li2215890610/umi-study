a98a52bacfac637f6b485ac5b3c9b1e5cbeeba96
yangDaXia <2215890610@qq.com>
2020-02-26 15:51:23 +0800

    bugfix

--- src/pages/Ui/Notification/Notification.js
+++ src/pages/Ui/Notification/Notification.js
@@ -30,19 +30,19 @@ class Notification extends React.Component {
       <div>
         <Card title='通知提醒框 (Notification)' className={styles.card}>
           <Row>
-            <Col span='4'>
+            <Col span={4}>
               <SuccessNotification />
             </Col>
-            <Col span='4'>
+            <Col span={4}>
               <ErrorNotification />
             </Col>
-            <Col span='4'>
+            <Col span={4}>
               <InfoNotification />
             </Col>
-            <Col span='4'>
+            <Col span={4}>
               <WarningNotification />
             </Col>
-            <Col span='4'>
+            <Col span={4}>
               <Button type="primary" onClick={this.notificationMessage}>自定义封装</Button>
             </Col>
 
