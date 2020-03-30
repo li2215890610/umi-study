916e2ce8c39bf28b4b5fb562c01fc231f998e8e1
yangDaXia <2215890610@qq.com>
2020-03-30 21:57:57 +0800

    增加无状态组件

--- src/pages/ReactComponent/Child.js
+++ src/pages/ReactComponent/Child.js
@@ -2,30 +2,45 @@
 import React, { Component } from 'react';
 
 
-class Child extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const Child = (props)=>{
 
-    }
-  }
-
-  render() {
-    console.log("我执行render了")
-    return (
-      <div style={{marginTop: 100}}>
-        我是子组件
-        `
-        {this.props.value}
-        `
-        <button onClick={()=>{
-          this.props.onClick && this.props.onClick(this.props.value ? this.props.value:"父组件收到值了")
-        }}>点我吧</button>
-      </div>
-    );
-  }
-
+  return (
+    <div style={{marginTop: 100}}>
+      我是子组件
+      `
+      {props.value}
+      `
+      <button onClick={()=>{
+        props.onClick && props.onClick(props.value ? props.value:"父组件收到值了")
+      }}>点我吧</button>
+    </div>
+  );
 }
+
+// class Child extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+
+//     }
+//   }
+
+//   render() {
+//     console.log("我执行render了")
+//     return (
+//       <div style={{marginTop: 100}}>
+//         我是子组件
+//         `
+//         {this.props.value}
+//         `
+//         <button onClick={()=>{
+//           this.props.onClick && this.props.onClick(this.props.value ? this.props.value:"父组件收到值了")
+//         }}>点我吧</button>
+//       </div>
+//     );
+//   }
+
+// }
 
 
 export default Child;
