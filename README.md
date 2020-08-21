ecd603f4756583b53cff4748c7284c3ac97e7f8f
yangDaXia <2215890610@qq.com>
2020-08-21 12:07:53 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,15 @@
+# umi3.0 study 
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
---
+++ config/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'umi';
+import routes from './routes';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: routes,
+});
